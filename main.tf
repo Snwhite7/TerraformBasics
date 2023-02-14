cbd10d666f0d86c6761122fa3e2518ff89a1a731
SteMa <snwitconsulting@gmail.com>
2023-02-14 11:35:11 -0600

    initial code

---
+++ main.tf
@@ -0,0 +1,36 @@
+# Azure Provider source and version being used
+terraform {
+    required_providers {
+        azurerm = {
+            source = {hashicorp/azurerm"
+            version = "=3.43.0"
+            }
+        }
+    }
+
+    # Configure the Microsoft Azure Provider
+    provider "azurerm" {
+        features {}
+    }
+
+# Create a resource group
+resource "azurerm_resource_group" "myrg" {
+    name = "stevew-rg"
+    location = "East US"
+}
+
+# Create a virtual network within the resource group
+resource "azurevm_virtual_network" "myvnet" {
+    name                         = "stevewhite-vnet"
+    resource_group_name          = azurerm_resource_group.myrg.name
+    location                     = azurerm_resource_group.myrg.location
+    address_space                = ["10.0.0.0/16"]         
+}
+
+# Create a subnet within the vnet
+resource "azurevm_subnet" "mysubnet" {
+    name                         = "stevewhite-subnet"
+    resource_group_name          = azurerm_resource_group.myrg.name
+    virtual_network_name         = azurerm_virtual_network.myvnet.name
+    address_prefixes             = ["10.0.0.0/24"]         ]
+}
