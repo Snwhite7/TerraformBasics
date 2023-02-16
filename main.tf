23d7b2664b4b67a84f421815887c417f89aae75c
SteMa <snwitconsulting@gmail.com>
2023-02-16 10:42:22 -0600

    fixed syntax

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 terraform {
     required_providers {
         azurerm = {
-            source = {hashicorp/azurerm"
+            source = "hashicorp/azurerm"
             version = "=3.43.0"
             }
         }
@@ -32,5 +32,5 @@ resource "azurevm_subnet" "mysubnet" {
     name                         = "stevewhite-subnet"
     resource_group_name          = azurerm_resource_group.myrg.name
     virtual_network_name         = azurerm_virtual_network.myvnet.name
-    address_prefixes             = ["10.0.0.0/24"]         ]
+    address_prefixes             = ["10.0.0.0/24"]         
 }
